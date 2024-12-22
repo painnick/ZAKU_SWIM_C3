ff380603f6c83a4aacdccf10545c2185490a486c
painnick <painnick@gmail.com>
2024-12-22 23:51:08 +0900

    INIT.

---
+++ src/common.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// These are all GPIO pins on the ESP32
+// Recommended pins include 2,4,12-19,21-23,25-27,32-33
+// for the ESP32-S2 the GPIO pins are 1-21,26,33-42
+
+// Base PIN & Channel
+#define PIN_SERVO1_TABLE 1 // OK
+#define PIN_EYE 7 // OK
+#define PIN_MOTOR1 20
+#define PIN_MOTOR2 21
+
+#define CH_EYE 3
---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+#include <Arduino.h>
+
+#define MAIN_TAG "Main"
+
+// Servo
+#define MAX_EASING_SERVOS 1
+#define DISABLE_MICROS_AS_DEGREE_PARAMETER
+
+#include <ServoEasing.hpp>
+
+// Controllers
+#include "common.h"
+
+#define EYE_NORMAL 23
+#define EYE_BRIGHT 221
+
+#define DEGREES_PER_SECOND 30
+
+ServoEasing bodyServo;
+
+void setup() {
+  ESP_LOGI(MAIN_TAG, "Setup!");
+
+  ESP_LOGI(MAIN_TAG, "Setup Servo");
+  bodyServo.setEasingType(EASE_QUADRATIC_IN);
+  bodyServo.attach(PIN_SERVO1_TABLE, 90);
+
+  ESP_LOGI(MAIN_TAG, "Setup Eye");
+  ledcSetup(CH_EYE, 1000, 8);
+  ledcAttachPin(PIN_EYE, CH_EYE);
+  ledcWrite(CH_EYE, EYE_NORMAL);
+
+  ESP_LOGI(MAIN_TAG, "Setup Legs");
+  pinMode(PIN_MOTOR1, OUTPUT);
+  pinMode(PIN_MOTOR2, OUTPUT);
+
+  digitalWrite(PIN_MOTOR1, HIGH);
+  digitalWrite(PIN_MOTOR2, LOW);
+}
+
+void loop() {
+  bodyServo.easeTo(0, DEGREES_PER_SECOND);
+  delay(2000);
+
+  bodyServo.easeTo(90, DEGREES_PER_SECOND);
+  digitalWrite(PIN_MOTOR1, LOW);
+  ledcWrite(CH_EYE, EYE_BRIGHT);
+  delay(1000);
+  ledcWrite(CH_EYE, EYE_NORMAL);
+  digitalWrite(PIN_MOTOR1, HIGH);
+
+  bodyServo.easeTo(180, DEGREES_PER_SECOND);
+  delay(2000);
+
+  bodyServo.easeTo(90, DEGREES_PER_SECOND);
+  digitalWrite(PIN_MOTOR1, LOW);
+  ledcWrite(CH_EYE, EYE_BRIGHT);
+  delay(3000);
+  ledcWrite(CH_EYE, EYE_NORMAL);
+  digitalWrite(PIN_MOTOR1, HIGH);
+}
